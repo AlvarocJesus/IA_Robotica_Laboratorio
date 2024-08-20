a5feef747c9592e7d95bdf3873100a175878833c
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-08-20 18:13:53 -0300

    Atualiza

--- lab1/chatbot.py
+++ lab1/chatbot.py
@@ -28,7 +28,7 @@ class ChatBot:
     lemmatizer = WordNetLemmatizer()
 
     def createModel(self):
-        nltk.download('punkt_tab')
+        nltk.download('punkt')
         nltk.download('wordnet')
         nltk.download('omw-1.4')
 
